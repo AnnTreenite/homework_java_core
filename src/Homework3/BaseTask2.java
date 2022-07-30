2ce140d5cb70e6e9b3e87667206167f71b5fc05a
Ann <ann.treenite@gmail.com>
2022-07-30 15:22:13 +0300

    fixed Base Task 2

--- src/Homework3/BaseTask2.java
+++ src/Homework3/BaseTask2.java
@@ -8,17 +8,17 @@ import java.io.IOException;
 
 public class BaseTask2 {
     public static void main(String[] args) throws IOException {
-
-        FileReader fileReader = new FileReader(" \\Users\\treenite\\IdeaProjects\\homework_java_core\\my_first_file.txt");
-         while (fileReader.ready()) {
-             char read = (char) fileReader.read();
-             System.out.print(read + " ");
-         }
+        FileReader fileReader = new FileReader("/Users/treenite/IdeaProjects/homework_java_core/my_first_file.txt");
         BufferedReader bufferedReader = new BufferedReader(fileReader);
-         while (bufferedReader.ready()) {
-             bufferedReader.readLine();
-             System.out.println(Line);
-         }
-
+        String text = "";
+        while (fileReader.ready()) {
+            char read = (char) fileReader.read();
+            if (read != '\n') {
+                text = text.concat(read + "");
+            }
+        }
+            fileReader.close();
+            bufferedReader.close();
+            System.out.print(text);
     }
 }
