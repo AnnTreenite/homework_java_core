b9f100eca7338708ac270d6ddee7584dc7e04130
Ann <ann.treenite@gmail.com>
2022-07-31 14:31:22 +0300

    убрала продвинутый =)

--- src/Homework3/CarTask/CarFactory.java
+++ src/Homework3/CarTask/CarFactory.java
@@ -2,18 +2,20 @@ package Homework3.CarTask;
 
 public class CarFactory {
     public static void main(String[] args) {
-        Toyota toyota = new Toyota();
-        Zhiguli zhiguli = new Zhiguli();
-
-//        int[] Toyota = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 , 16, 17, 18, 19, 20};
-//        int[] Zhiguli = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 , 16, 17, 18, 19, 20};
-        Car[] carsArray = new Car[20];
-        carsArray[i] = CarFactory.createToyota();
-
-        Car[] cars = {toyota, zhiguli};
-        for (Car car : cars) {
-            Car car1 = (Car) car;
-            car1.theyExist();
-        }
+////        Toyota toyota = new Toyota();
+////        Zhiguli zhiguli = new Zhiguli();
+//
+////        int[] Toyota = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 , 16, 17, 18, 19, 20};
+////        int[] Zhiguli = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 , 16, 17, 18, 19, 20};
+//
+//        Car[] carsArray = new Car[20];
+//        int i = 10;
+////        carsArray[i] = CarFactory.createToyota();
+//
+//        Car[] cars = {toyota, zhiguli};
+//        for (Car car : cars) {
+//            Car car1 = (Car) car;
+//            car1.theyExist();
+//        }
     }
 }
